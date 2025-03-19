49dbd4fb4d7cce20f9609f61a59a03a3a487d36a
Ben <bgoober2@protonmail.com>
2025-03-19 03:07:02 -0400

    FE: add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </ClusterProvider>
         </ReactQueryProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
