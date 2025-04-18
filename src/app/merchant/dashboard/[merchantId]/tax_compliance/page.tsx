8d1a382e778d9bcd51dc0694727dafdee5798639
Ben <bgoober2@protonmail.com>
2025-04-18 00:29:36 -0400

    edit page

--- src/app/merchant/dashboard/[merchantId]/tax_compliance/page.tsx
+++ src/app/merchant/dashboard/[merchantId]/tax_compliance/page.tsx
@@ -28,26 +28,26 @@ export default function ManageAccountsPage({ params }: { params: { merchantId: s
           </p>
           <br></br>
           <p className="text-gray-500">
-            What you see now is modeled on the US Virgin Islands 5% tax on a business's monthly gross receipts.
+            What you see now is modeled on the US Virgin Islands 5% tax on a business&apos;s monthly gross receipts.
             Different businesses will fall into different tax brackets, but for now we are mocking a tax burden of 5% of gross revenue, paid monthly.
             Here is how it currently works:
           </p>
           <br></br>
           <ul className="list-disc pl-5 space-y-2 text-gray-500">
             <li>
-              When the Owner withdraws USDC from the Merchant's account, 5% of the withdrawal amount is deposited into a separate escrow dedicated to tax/compliance payments.
+              When the Owner withdraws USDC from the Merchant&apos;s account, 5% of the withdrawal amount is deposited into a separate escrow dedicated to tax/compliance payments.
             </li>
             <li>
-              The other 93.5% goes to the Merchant's Owner's USDC account, and 1.5% goes to the House's platform fee.
+              The other 93.5% goes to the Merchant&apos;s Owner&apos;s USDC account, and 1.5% goes to the House&apos;s platform fee.
             </li>
-            <li>The 5% diverted to the Merchant's compliance escrow continues to accrue USDC each time the Merchant's Owner withdraws from the Merchant's USDC account.</li>
+            <li>The 5% diverted to the Merchant&apos;s compliance escrow continues to accrue USDC each time the Merchant&apos;s Owner withdraws from the Merchant&apos;s USDC account.</li>
             <li>
-              Once the time comes to pay their monthly tax bill, the Owner can click the "Pay The Man" button and 100% of the Merchant's compliance escrow will be paid to THE MAN's USDC account.
+              Once the time comes to pay their monthly tax bill, the Owner can click the &quot;Pay The Man&quot; button and 100% of the Merchant&apos;s compliance escrow will be paid to THE MAN&apos;s USDC account.
             </li>
-            <li>This achieves a preliminary level of automation regarding the disbersion and allocation of revenues, while ensuring all 3 interested parties are either paid, or earmarked funds simultaneously (The Merchant's Owner, the House, and THE MAN); all during the withdrawal of USDC from the Merchant's account.</li>
+            <li>This achieves a preliminary level of automation regarding the disbersion and allocation of revenues, while ensuring all 3 interested parties are either paid, or earmarked funds simultaneously (The Merchant&apos;s Owner, the House, and THE MAN); all during the withdrawal of USDC from the Merchant&apos;s account.</li>
           </ul>
           <br></br>
-          <p>We have decided to build and showcase this feature as an example of WHAT COULD BE possible, or a direction that we'd like to begin a conversation around and work towards; both on the Merchant's end, and the Government/Tax Authority's end. This assumes that the Government and relevant departments have been onboarded with their own accounts, and are willing to accept tax payments in this manner.</p>
+          <p>We have decided to build and showcase this feature as an example of WHAT COULD BE possible, or a direction that we&apos;d like to begin a conversation around and work towards; both on the Merchant&apos;s end, and the Government/Tax Authority&apos;s end. This assumes that the Government and relevant departments have been onboarded with their own accounts, and are willing to accept tax payments in this manner.</p>
         </div>
 
       </div>
